5aa94a685e2d03b4ee26d9c92ac4edcf1ddc772e
spects4 <spects4@gmail.com>
2016-08-03 11:17:50 -0400

    Add files via upload

---
+++ app.R
@@ -0,0 +1,65 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <-shinyUI(bootstrapPage(
+  
+  selectInput(inputId = "n_breaks",
+              label = "Number of bins in histogram (approximate):",
+              choices = c(10, 20, 35, 50),
+              selected = 20),
+  
+  checkboxInput(inputId = "individual_obs",
+                label = strong("Show individual observations"),
+                value = FALSE),
+  
+  checkboxInput(inputId = "density",
+                label = strong("Show density estimate"),
+                value = FALSE),
+  
+  plotOutput(outputId = "main_plot", height = "300px"),
+  
+  # Display this only if the density is shown
+  conditionalPanel(condition = "input.density == true",
+                   sliderInput(inputId = "bw_adjust",
+                               label = "Bandwidth adjustment:",
+                               min = 0.2, max = 2, value = 1, step = 0.2)
+  )
+  
+))
+
+# Define server logic required to draw a histogram
+server <- shinyServer(function(input, output) {
+  
+  output$main_plot <- renderPlot({
+    
+    hist(faithful$eruptions,
+         probability = TRUE,
+         breaks = as.numeric(input$n_breaks),
+         xlab = "Duration (minutes)",
+         main = "Geyser eruption duration")
+    
+    if (input$individual_obs) {
+      rug(faithful$eruptions)
+    }
+    
+    if (input$density) {
+      dens <- density(faithful$eruptions,
+                      adjust = input$bw_adjust)
+      lines(dens, col = "blue")
+    }
+    
+  })
+})
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
